965e2b4b0662e9a1f8499f0b34d439081daa92a9
hugozeballos <Hzeballosmeneses@gmail.com>
2025-07-31 00:49:09 -0400

    pruebas login produccion no funcional

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,9 +5,12 @@ from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class TranscribeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
     parser_classes = [MultiPartParser]
 
     def post(self, request, *args, **kwargs):
